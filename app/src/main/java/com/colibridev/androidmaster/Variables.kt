19dad20358afee7344f4934a49fba3bf0fbd3b74
JG <piedradesales@gmail.com>
2023-07-24 21:07:10 +0200

    Primeros pasos con Android

---
+++ app/src/main/java/com/colibridev/androidmaster/Variables.kt
@@ -0,0 +1,42 @@
+package com.colibridev.androidmaster
+
+//Comentario
+/**
+ * Comentario de varias líneas
+ */
+
+fun main(){
+
+    // val (constantes)
+
+    //Int -2.147  a 2.147.483.647
+    val age:Int = 23
+
+    //Float
+    val floatExample:Float = 2003.212f
+
+    //Double
+    val doubleExample:Double = 23332.23
+
+    //Char
+    val charExample:Char = 'a'
+    // String "case camel"
+    val stringExample:String = "ColbríDev suscribete"
+
+    //Boolean
+    val booleanExampleA:Boolean = true
+    val booleanExampleB:Boolean = false
+
+    println(stringExample)
+    println(booleanExampleA)
+
+    //var (variables)
+    var age2:Int = age
+
+    println(age2)
+
+    age2 = 200
+
+    print(age2)
+}
+
